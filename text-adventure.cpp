edc042faee43d7c3af0f76b6d4df9cd788f3d641
maria-osipova <maryosipovaa@gmail.com>
2025-01-11 18:11:41 -0300

    story continues

--- text-adventure.cpp
+++ text-adventure.cpp
@@ -33,23 +33,24 @@ int main() {
         }
     }
 
-    std::cout << "well, lets start then...\n";
+    std::cout << "well, lets start then... press enter to continue.\n";
     {
         std::string dummy;
         std::getline(std::cin, dummy);
     }
 
+    std::string gender;
+
     while (true) {
-        std::cout << "first of all i would ask you who you wanna be today. are you a man or a woman? type your answer. \n";
+        std::cout << "first of all i would like to ask you: who do you wanna be today? are you a man or a woman? type your answer. \n";
 
-        std::string response;
-        std::getline(std::cin, response);
+        std::getline(std::cin, gender);
 
-        if (response == "man") {
+        if (gender == "man") {
             std::cout << "good job! let's continue.\n";
             break;
         }
-        else if (response == "woman") {
+        else if (gender == "woman") {
             std::cout << "good job! let's continue.\n";
             break;
         }
@@ -58,5 +59,32 @@ int main() {
         }
     }
 
+    std::cout << "enter your name.\n";
+    std::string name;
+
+    while (true) {
+
+        std::getline(std::cin, name);
+
+        if (!name.empty() && name.find_first_not_of(" \t") != std::string::npos) {
+
+            std::cout << "so. the story begins. you are a " << gender << ", named " << name << ". press enter to continue.\n";
+            std::string dummy;
+            std::getline(std::cin, dummy);
+            break;
+        }
+        else {
+            std::cout << "hmm.. I'm not sure if you typed your name. try again.\n";
+        }
+    }
+
+
+    std::cout << "now please enter the country you live in.\n";
+
+    std::cout << "";
+
     return 0;
 }
+
+
+
